28a1a54cc5c57c252260823fca098a2d643c9c9c
unknown <2427079555@qq.com>
2023-01-11 13:12:57 +0800

    cheng

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="cover" :class="{focusCover:focusAn}"></div>
-  <img
-    :class="{focusImg:focusAn}"
-    id="bgImg"
-    :src="imgUrl"
-    alt=""
-  />
+  <div class="cover" :class="{ focusCover: focusAn }"></div>
+  <img :class="{ focusImg: focusAn }" id="bgImg" :src="imgUrl" alt="" />
   <div class="titleBar">
     <div class="title">
-      <img src="../assets/vue.svg" alt="">
+      <img src="../assets/vue.svg" alt="" />
       <span>塔塔开-bili-download</span>
     </div>
     <div class="icons">
@@ -19,60 +14,75 @@
     </div>
   </div>
   <div class="iconBox">
-    <el-badge :hidden="isDown" v-if="$route.path!='/download'" is-dot class="item"><i @click="goDownloading" class="iconfont icon-yunduanxiazai"></i></el-badge>
-    <router-link to="/home"><i v-if="$route.path!='/home'" class="iconfont icon-zhuye"></i></router-link>
-    <router-link to="/setup"><i v-if="$route.path!='/setup'" class="iconfont icon-shezhi"></i></router-link>
+    <el-badge
+      :hidden="isDown"
+      v-if="$route.path != '/download'"
+      is-dot
+      class="item"
+      ><i @click="goDownloading" class="iconfont icon-yunduanxiazai"></i
+    ></el-badge>
+    <router-link to="/home"
+      ><i v-if="$route.path != '/home'" class="iconfont icon-zhuye"></i
+    ></router-link>
+    <router-link to="/setup"
+      ><i v-if="$route.path != '/setup'" class="iconfont icon-shezhi"></i
+    ></router-link>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
-import { ipcRenderer } from 'electron';
-import {useRouter} from 'vue-router'
-const path = require('path')
+import { ref } from "vue";
+import { ipcRenderer } from "electron";
+import { useRouter } from "vue-router";
+const path = require("path");
 
-const router = useRouter()
+const router = useRouter();
 
 const props = defineProps({
-  focusAn:{
-    type:Boolean,
-    required:false,
-    default:false
+  focusAn: {
+    type: Boolean,
+    required: false,
+    default: false,
   },
-  isDown:{
-    type:Boolean,
-    default:true
-  }
-})
-const badgeShow=ref(props.isDown)
+  isDown: {
+    type: Boolean,
+    default: true,
+  },
+});
+const badgeShow = ref(props.isDown);
 
-let imgName = localStorage.getItem('newImg')?'current.jpg':'defaultImg.jpg'
-const imgUrl = new URL(path.join(path.resolve(),`resources/src/assets/${imgName}`), import.meta.url).href+'?'+localStorage.getItem('endArg')
+let imgName = localStorage.getItem("newImg") ? "current.jpg" : "defaultImg.jpg";
+const imgUrl =
+  new URL(
+    path.join(path.resolve(), `resources/src/assets/${imgName}`),
+    import.meta.url
+  ).href +
+  "?" +
+  localStorage.getItem("endArg");
 // const imgUrl = new URL(`../assets/${imgName}`, import.meta.url).href+'?'+localStorage.getItem('endArg')
-const isMax = ref(false)
-function toMini(){
-  ipcRenderer.send('mini-app')
+const isMax = ref(false);
+function toMini() {
+  ipcRenderer.send("mini-app");
 }
-function toMax(){
-  console.log('max');
-  ipcRenderer.send('max-app')
+function toMax() {
+  console.log("max");
+  ipcRenderer.send("max-app");
   // isMax.value = true
 }
-function toClose(){
-  ipcRenderer.send('close-app')
+function toClose() {
+  ipcRenderer.send("close-app");
 }
-function toNormal(){
-  ipcRenderer.send('normal-app')
+function toNormal() {
+  ipcRenderer.send("normal-app");
 }
-ipcRenderer.on('winResize',()=>isMax.value = !isMax.value)
-function goDownloading(){
-  router.push('/download')
+ipcRenderer.on("winResize", () => (isMax.value = !isMax.value));
+function goDownloading() {
+  router.push("/download");
 }
-
 </script>
 
 <style lang="scss" scoped>
-.titleBar{
+.titleBar {
   position: absolute;
   top: 0;
   height: 25px;
@@ -82,34 +92,34 @@ function goDownloading(){
   color: rgb(255, 255, 255);
   font-size: 13px;
   background-color: rgba(0, 0, 0, 0.103);
-  .title{
+  .title {
     display: flex;
     align-items: center;
     margin-left: 3px;
     -webkit-app-region: drag;
     width: calc(100% - 150px);
-    img{
+    img {
       width: 20px;
       height: 20px;
       border-radius: 50%;
     }
-    span{
+    span {
       margin-left: 5px;
     }
   }
-  .icons{
+  .icons {
     display: flex;
     align-items: center;
-    i{
+    i {
       display: block;
       width: 50px;
       cursor: pointer;
       font-size: 18px;
-      transition: .25s;
-      &:last-child{
+      transition: 0.25s;
+      &:last-child {
         font-size: 14px;
       }
-      &:hover{
+      &:hover {
         background-color: rgb(148, 148, 148);
       }
     }
@@ -129,7 +139,6 @@ function goDownloading(){
   transform: scale(1.1);
 }
 .cover {
-  
   width: 100%;
   height: 100%;
   background-image: radial-gradient(rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, 0.5) 100%),
